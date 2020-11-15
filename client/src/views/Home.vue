bf5c4274d88b333d0ee220706c17d0ce84628f61
MounirBlk <mounir.ballouk@gmail.com>
2020-11-15 21:50:44 +0100

    avancement socket, a revoir cors bug, vid 30:00

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,59 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+    <div class="header">
+      <h1>Chatroom</h1>
+      <p class="username">Username: {{ username }}</p>
+      <p class="online">Online: {{ users.length }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import io from 'socket.io-client';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data: () => {
+    return{
+      username: null,
+      socket: io("http://localhost:4000"),
+      messages: [],
+      users: []
+    }
+  },
+  created() {},
+  mounted() {
+    //this.username = prompt('Quel est votre pseudo ?', 'Anonymous');
+    if(!this.username) this.username = 'Anonymous';//this.username = !this.username ? 'Anonymous' : this.username
+    this.joinServer();
+  },
+  methods: {
+    joinServer: function(){
+      this.socket.on('loggedIn', data => {
+        this.messages = data.messages;
+        this.users = data.users;
+        this.socket.emit('newuser',this.username);
+      });
+    }
+  },
 }
 </script>
+<style lang="scss">
+body{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #2C3E50;
+  margin:0;
+  padding:0;
+}
+
+#home{
+  display:flex;
+  flex-direction:column;
+  height: 100vh;
+  width: 100%;
+  max-width: 768px;
+  margin:0 auto;
+}
+</style>
